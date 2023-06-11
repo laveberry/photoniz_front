60aca5843b2127d79eb9cf1341172aca1e687731
laveberry <laveberry7@gmail.com>
2023-06-11 12:05:31 +0900

    포토게시판 모듈화

--- src/views/Photo.js
+++ src/views/Photo.js
@@ -1,5 +1,6 @@
 import React,{useState, useEffect} from "react";
 import Paging from "components/Paging/Paging";
+import PhotoTable from "components/Table/PhotoTable";
 
 // reactstrap components
 import {
@@ -17,31 +18,41 @@ import PanelHeader from "components/PanelHeader/PanelHeader.js";
 
 import { thead, tbody } from "variables/photo";
 
-function RegularTables() {
+
+
+function Photo() {
 
   //페이징 시작
-  const [products, setProducts] = useState([tbody]);  // 리스트에 나타낼 아이템들
-  const [count, setCount] = useState(0); // 아이템 총 개수
-  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지. default 값으로 1
-  const [postPerPage] = useState(5); // 한 페이지에 보여질 아이템 수 
-  const [indexOfLastPost, setIndexOfLastPost] = useState(0); // 현재 페이지의 마지막 아이템 인덱스
-  const [indexOfFirstPost, setIndexOfFirstPost] = useState(0); // 현재 페이지의 첫번째 아이템 인덱스
-  const [currentPosts, setCurrentPosts] = useState(0); // 현재 페이지에서 보여지는 아이템들
+  // const [products, setProducts] = useState([tbody]);  // 리스트에 나타낼 아이템들
+  // const [count, setCount] = useState(0); // 아이템 총 개수
+  // const [currentPage, setCurrentPage] = useState(1); // 현재 페이지. default 값으로 1
+  // const [postPerPage] = useState(5); // 한 페이지에 보여질 아이템 수 
+  // const [indexOfLastPost, setIndexOfLastPost] = useState(0); // 현재 페이지의 마지막 아이템 인덱스
+  // const [indexOfFirstPost, setIndexOfFirstPost] = useState(0); // 현재 페이지의 첫번째 아이템 인덱스
+  // const [currentPosts, setCurrentPosts] = useState(0); // 현재 페이지에서 보여지는 아이템들
 
-  useEffect(() => {
-    // axios
-    //   .get("/product-sale-join")
-    //   .then((res) => {
-    //     setProducts(res.data);
-    //   })
-    //   .catch((error) => {
-    //     console.log("error", error);
-    //   });
-    setCount(products.length);
-    setIndexOfLastPost(currentPage * postPerPage);
-    setIndexOfFirstPost(indexOfLastPost - postPerPage);
-    setCurrentPosts(products.slice(indexOfFirstPost, indexOfLastPost));
-  }, [currentPage, indexOfLastPost, indexOfFirstPost, products, postPerPage]);
+  // useEffect(() => {
+  //   // axios
+  //   //   .get("/product-sale-join")
+  //   //   .then((res) => {
+  //   //     setProducts(res.data);
+  //   //   })
+  //   //   .catch((error) => {
+  //   //     console.log("error", error);
+  //   //   });
+  //   setCount(products.length);
+  //   setIndexOfLastPost(currentPage * postPerPage);
+  //   setIndexOfFirstPost(indexOfLastPost - postPerPage);
+  //   setCurrentPosts(products.slice(indexOfFirstPost, indexOfLastPost));
+  // }, [currentPage, indexOfLastPost, indexOfFirstPost, products, postPerPage]);
+
+    //props한번에 전달
+    const propData = {
+      tbody : tbody,
+      thead : thead,
+      count : 0,
+      postPerPage : 10,
+    }
 
   const setPage = (error) => {
     setCurrentPage(error);
@@ -49,56 +60,9 @@ function RegularTables() {
   //페이징 끝
   
   return (
-    <>
-      <PanelHeader size="sm" />
-      <div className="content">
-        <Row>
-          <Col xs={12}>
-            <Card>
-              <CardHeader>
-                <CardTitle tag="h4">사진</CardTitle>
-              </CardHeader>
-              <CardBody>
-                <Table responsive>
-                  <thead className="text-primary">
-                    <tr>
-                      {thead.map((prop, key) => {
-                        if (key === thead.length - 1)
-                          return (
-                            <th key={key} className="text-right">
-                              {prop}
-                            </th>
-                          );
-                        return <th key={key}>{prop}</th>;
-                      })}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {tbody.map((prop, key) => {
-                      return (
-                        <tr key={key}>
-                          {prop.data.map((prop, key) => {
-                            if (key === thead.length - 1)
-                              return (
-                                <td key={key} className="text-right">
-                                  {prop}
-                                </td>
-                              );
-                            return <td key={key}>{prop}</td>;
-                          })}
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </Table>
-                <Paging page={currentPage} count={count} setPage={setPage}/>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    </>
+    // <PhotoTable count={count} tbody={tbody} thead={thead} postPerPage={postPerPage}/>
+    <PhotoTable {...propData}/>
   );
 }
 
-export default RegularTables;
+export default Photo;
